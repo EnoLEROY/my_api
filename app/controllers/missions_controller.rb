4b8005fceebbd513bf4c8f506ec3188f2fd8da7a
Eno LEROY <eno.leroy@gmail.com>
2023-06-23 16:18:41 +0200

    api finished

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -1,10 +1,11 @@
 class MissionsController < ApplicationController
 
   def index
-    @missions = Mission.all
-    @missions_json = {
-      "missions": @missions
+    @missions = Mission.pluck(:listing_id, :mission_type, :date, :price)
+      .map { |listing_id, mission_type, date, price|
+      { listing_id: listing_id, mission_type: mission_type, date: date, price: price }
     }
+    @missions_json = { "missions": @missions }
     render json: @missions_json
   end
 end
